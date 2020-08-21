9ad9073de598ff9f442b7131adcf3362e346243f
sourabh chauhan <sourabh1202@gmail.com>
2020-08-21 14:53:16 +0530

    Updates

---
+++ precache-manifest.ee3761b61661292177d28ae50fd98c53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ebe022db5652436a025f6d86851540",
+    "url": "/SimpleAnimationReactJS/index.html"
+  },
+  {
+    "revision": "0233d5441e5b8d2786c0",
+    "url": "/SimpleAnimationReactJS/static/css/main.c88d5784.chunk.css"
+  },
+  {
+    "revision": "da84de451038f869b24a",
+    "url": "/SimpleAnimationReactJS/static/js/2.26eabddb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SimpleAnimationReactJS/static/js/2.26eabddb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0233d5441e5b8d2786c0",
+    "url": "/SimpleAnimationReactJS/static/js/main.6f42c6e6.chunk.js"
+  },
+  {
+    "revision": "8af1523bfd266f3810d2",
+    "url": "/SimpleAnimationReactJS/static/js/runtime-main.c97ae666.js"
+  }
+]);
